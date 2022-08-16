fad39fb812a772fe6315a2500d588578ca71a665
Victor Choe <vchoe89@gmail.com>
2022-08-15 20:10:19 -0400

    data-finance

---
+++ src/Components/Analytics.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpeg'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, facere earum! Quisquam veniam illo perferendis! Autem culpa iure maiores laboriosam error facilis nihil? Optio alias non dicta tempora dolorem asperiores?</p>
+                <button className='bg-black w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a]'>Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Analytics
